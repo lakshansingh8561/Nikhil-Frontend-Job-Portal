ea7fb835c74b60fce2910d933511a169adbb1372
Lakshan Singh <lakshansingh8561@gmail.com>
2026-07-28 16:10:45 +0530

    post change

--- src/Redux/api/baseApi.ts
+++ src/Redux/api/baseApi.ts
@@ -5,7 +5,7 @@ export const baseApi = createApi({
   reducerPath: "api",
 
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1/",
+    baseUrl: "https://nikhil-backend-job-portal.vercel.app/api/v1/",
 
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as RootState).auth.accessToken;
